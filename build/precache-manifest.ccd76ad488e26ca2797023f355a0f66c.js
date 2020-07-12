1d450e330c0a436472f84583f7f7e7d38cfc2e91
Thierry Oke <thierryoke@Thierrys-MacBook-Air.local>
2020-07-12 11:17:36 -0500

    changes

---
+++ build/precache-manifest.ccd76ad488e26ca2797023f355a0f66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbc3a5ba034e1e0e1137afa73a57583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bba7e6d429c1a988eb4",
+    "url": "/static/css/main.da6ccbbf.chunk.css"
+  },
+  {
+    "revision": "68fbc58a035c2a3fb722",
+    "url": "/static/js/2.255ee09b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.255ee09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bba7e6d429c1a988eb4",
+    "url": "/static/js/main.1192292e.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
